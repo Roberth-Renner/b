4ee0c50948b554cd35e96a63a2d536aac907cb55
roberthRenner <133983214+Roberth-Renner@users.noreply.github.com>
2023-08-31 17:39:07 -0300

    Add files via upload
    Realizei a separação das partes que são definidas Modelo Relacional, Criação de Tabelas, Inserção dos dados, e a Manipulação dos dados.

---
+++ Criando as tabelas.sql
@@ -0,0 +1,156 @@
+
+                                                             --    SCRIPT COM A CRIAÇÃO DAS TABELAS    --
+
+show databases;
+create database ecommerce;
+use ecommerce;
+
+
+-- REALIZEI ALGUMAS ALTERAÇÕES NO CÓDIGO, ALGUNS DADOS E TABELAS ADICIONAIS --
+
+-- CRIANDO A TABELA DE CLIENTES --
+
+
+
+create table Cliente(
+	IdCliente int auto_increment primary key,
+    Fnome varchar(10) not null,
+    Mnome char(1) ,
+    Lnome varchar(10) not null,
+    Endereco varchar(255) not null,
+    TipoDocumento ENUM('PF', 'PJ') not null,
+    CPF char(11) unique,
+    CNPJ char(14) unique,
+    DataNascimento date not null
+);
+
+
+-- CRIANDO TABELA DE PAGAMENTO --
+
+create table Pagamento(
+	IdPagamento int auto_increment,
+    IdCliente int,
+    TipoPagamento enum('Cartao', 'Boleto', 'Pix', 'Pagamento nao realizado') default ('Pagamento nao realizado'),
+    constraint primary key (IdPagamento, IdCliente),
+    constraint fk_Cliente_Pagamento foreign key (IdCliente) references Cliente(IdCliente)
+);
+
+-- CRIANDO A TABELA PEDIDOS --
+
+create table Pedidos(
+	IdPedido int auto_increment primary key,
+    IdCliente int,
+    Descricao varchar(250),
+    StatusPedido enum('Pedido realizado', 'Em andamento', 'Saiu para entrega', 'Finalizado', 'Esperando pagamento') default ('Esperando pagamento'),
+    Frete float,
+    CupomFrete boolean default 0,
+    IdPagamento int,
+    Quantidade int not null,
+    PreçoUnidade float not null,
+    constraint fk_Pagamento_Pedidos foreign key (IdPagamento) references Pagamento(IdPagamento),
+    constraint fk_Cliente_Pedidos foreign key (IdCliente) references Cliente(IdCliente)
+);
+
+-- CRIANDO TABELA DE ENTREGA --
+
+create table Entrega(
+	IdEntrega int auto_increment,
+    IdCliente int,
+    IdPedido int,
+    Estatus enum('Saiu para entrega', 'Entrega realizada', 'Preparando pedido') default ('Preparando pedido'),
+    TipoDeEntrega enum('Urgencia', 'Normal') default ('Normal'),
+    CodigoRastreio CHAR(10),
+    constraint primary key (IdEntrega, IdCliente, IdPedido),
+    constraint fk_Entrega_Pedido foreign key (IdPedido) references Pedidos(IdPedido),
+    constraint fk_Entrega_Cliente foreign key (IdCliente) references Cliente(IdCliente)
+);
+
+-- CRIANDO TABELA DE PRODUTO --
+
+create table Produto(
+	IdProduto int auto_increment primary key,
+    Categoria enum('Eletrônicos', 'Eletrodomésticos', 'Moveis', 'Alimentos', 'Utensilios'),
+    ClassificacaoCrianca boolean,
+    PNome varchar(30) not null,
+    Avaliacao int check (Avaliacao >= 0 and Avaliacao <= 5),
+    Pdimensao varchar(25)
+);
+
+-- CRIANDO TABELA DE ESTOQUE --
+
+create table Estoque(
+	IdEstoque int auto_increment primary key,
+    Elocal varchar(250) not null,
+    Ecategoria enum('Eletrônicos', 'Eletrodomésticos', 'Moveis', 'Alimentos', 'Utensilios') not null,
+    Quantidade int not null
+);
+
+-- CRIANDO TABELA DE FORNECEDOR --
+
+create table Fornecedor(
+	IdFornecedor int auto_increment primary key,
+    Flocal varchar(250) not null,
+    CNPJ char(14) not null unique,
+    RazaoSocial varchar(20) not null,
+    Contato char(11) not null,
+    AvaliacaoFornecedor int check(AvaliacaoFornecedor >= 0 and AvaliacaoFornecedor <= 5)
+);
+
+
+-- CRIANDO TABELA DE VENDEDOR --
+
+create table Vendedor(
+	IdVendedor int auto_increment primary key,
+    Vlocal varchar(250) not null,
+    CNPJ char(14) not null unique,
+    RazaoSocial varchar(40) not null,
+    Contato char(11) not null,
+    NomeFantasia varchar(25),
+    AvaliacaoVendedor int check(AvaliacaoVendedor >= 0 and AvaliacaoVendedor <= 5)
+);
+
+-- CRIANDO TABELAS DE RELAÇÃO PARA EFETUAR A SOLUÇÃO DAS RELAÇÕES M PARA N --
+
+-- CRIANDO TABELA DA RELAÇÃO PRODUTO/VENDEDOR -- 
+
+create table Relacao_Prod_Vend(
+	IdRelacaoVend int auto_increment,
+    IdProduto int,
+    IdVendedor int,
+    constraint primary key(IdRelacaoVend, IdProduto, IdVendedor),
+    constraint fk_Vendedor_Produto foreign key (IdVendedor) references Vendedor(IdVendedor),
+    constraint fk_Produto_Vendedor foreign key (IdProduto) references Produto(IdProduto) 
+);
+
+-- CRIANDO TABELA DA RELAÇÃO PRODUTO/FORNECEDOR --
+
+create table Relacao_Prod_Forn(
+	IdRelacaoForn int auto_increment,
+    IdProduto int,
+    IdFornecedor int,
+    constraint primary key (IdRelacaoForn, IdProduto, IdFornecedor),
+    constraint fk_Fornecedor_Produto foreign key (IdFornecedor) references Fornecedor(IdFornecedor),
+    constraint fk_Produto_Fornecedor foreign key (IdProduto) references Produto(IdProduto) 
+);
+
+-- CRIANDO TABELA DA RELAÇÃO PRODUTO/ESTOQUE --
+
+create table Relacao_Prod_Est(
+	IdRelacaoEstoq int auto_increment,
+    IdProduto int,
+    IdEstoque int,
+    constraint primary key (IdRelacaoEstoq, IdProduto, IdEstoque),
+    constraint fk_Estoque_Produto foreign key (IdEstoque) references Estoque(IdEstoque),
+    constraint fk_Produto_Estoque foreign key (IdProduto) references Produto(IdProduto) 
+);
+
+-- CRIANDO TABELA DA RELAÇÃO PRODUTO/PEDIDO --
+
+create table Realacao_Prod_Pedid(
+	IdRelacaoPedid int auto_increment,
+    IdProduto int,
+    IdPedido int,
+    constraint primary key (IdRelacaoPedid, IdProduto, IdPedido),
+    constraint fk_Pedido_Produto foreign key (IdPedido) references Pedidos(IdPedido),
+    constraint fk_Produto_Pedido foreign key (IdProduto) references Produto(IdProduto) 
+);
---
+++ Manipulação dos Dados.sql
@@ -0,0 +1,68 @@
+                                            
+                                              --    SCRIPT COM A MANIPULAÇÃO E ANALISE DOS DADOS    --
+
+-- SELEÇÃO SIMPLES COM SELECT PARA MOSTRAR CLIENTES CADASTRADOS COM CPF --
+
+select concat(Fnome,' ', Lnome) as NomeCompleto, Endereco, CPF  from Cliente where CPF is not null;
+
+-- SELEÇÃO PARA MANIPULAR OS TIPOS DE CATEGORIAS DO PEDIDO --
+
+select concat(Fnome,' ', Lnome) as NomeCompleto, Descricao, Categoria
+	   from Produto
+       inner join Realacao_Prod_Pedid Using (IdProduto)
+       inner join Pedidos Using(IdPedido)
+       inner join Cliente Using(Idcliente)
+       where Categoria like 'Eletrônicos%' ;
+
+-- CLIENTES QUE REALIZARAM O PAGAMENTO E ESTÃO NO ESTADO DE ENTREGA --
+
+select concat(Fnome,' ', Lnome) as Nome_Completo, Estatus as Status_Pedido, TipoDeEntrega, Descricao, Quantidade, Frete, PreçoUnidade, round(((PreçoUnidade*Quantidade) + Frete), 2) as Total_a_Pagar
+		from Pedidos p
+        inner join Entrega e on p.IdPedido = e.IdPedido 
+        inner join Cliente c on e.IdCliente = c.IdCliente
+        order by PreçoUnidade desc;
+        
+-- CLIENTES QUE COMPRARAM COM VALORES ENTRE 500 E 5000 --
+        
+select concat(Fnome,' ', Lnome) as Nome_Completo, Estatus as Status_Pedido, TipoDeEntrega, Descricao, Quantidade, Frete, PreçoUnidade, round(((PreçoUnidade*Quantidade) + Frete), 2) as Total_a_Pagar
+		from Pedidos p
+        inner join Entrega e on p.IdPedido = e.IdPedido 
+        inner join Cliente c on e.IdCliente = c.IdCliente
+        where (PreçoUnidade between 500 and 5000);
+        
+-- QUANTIDADE DE PEDIDOS FEITO POR CADA CLIENTE --
+
+select IdCliente, concat(Fnome,' ', Lnome) as Nome_Completo, count(*) as QuantidadePedidos
+	   from Pedidos p
+       join Cliente c using(IdCliente)
+       group by IdCliente;
+
+-- VERIFICANDO SE ALGUM FORNECEDOR TAMBÉM É VENDEDOR --
+
+select  v.CNPJ as CNPJ_Vendedor, v.RazaoSocial as RazaoSocial_Vendedor, f.CNPJ as CNPJ_Fornecedor, f.RazaoSocial as RazaoSocial_Fornecedor
+		from Fornecedor as f
+        join Vendedor as v on v.CNPJ = f.CNPJ;
+        
+-- RELAÇÃO DE PRODUTOS FORNECEDORES E ESTOQUE --
+
+select p.Categoria, p.Pnome as Nome_Produto,RazaoSocial,CNPJ as CNPJ_FORNECEDOR, f.AvaliacaoFornecedor, Quantidade as Quantidade_em_Estoque, Elocal as Locacao_Estoque
+	   from fornecedor f
+       join Relacao_Prod_Forn pf using(IdFornecedor)
+       join Produto p using(IdProduto)
+       join Relacao_Prod_Est pe using(IdProduto)
+       join Estoque e using(IdEstoque) 
+       order by AvaliacaoFornecedor desc;
+       
+-- RELACAO DE NOMES DOS PRODUTOS COM OS NOMES DAS FORNCEDORAS --       
+
+select Pnome as Nome_Produto, Categoria, RazaoSocial as Nome_Fornecedor, CNPJ, Contato_F
+	   from Fornecedor
+       join Relacao_Prod_Forn using(IdFornecedor)
+       join Produto using(IdProduto);
+        
+        
+        
+        
+        
+        
+        
